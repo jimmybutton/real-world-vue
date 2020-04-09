82499f247a6846eef299f42a062025006825b25f
Samuel Liedtke <samu.liedtke@gmail.com>
2020-04-09 17:47:54 +0100

    event module added

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import EventService from '@/services/EventService'
+import * as user from '@/store/modules/user.js'
+import * as event from '@/store/modules/event.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    user,
+    event
+  },
   state: {
-    user: { id: 'abc123', name: 'Adam Jahr' },
     categories: [
       'sustainability',
       'nature',
@@ -15,24 +19,6 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
-    ],
-    events: []
-  },
-  mutations: {
-    ADD_EVENT(state, event) {
-      state.events.push(event)
-    }
-  },
-  actions: {
-    createEvent({ commit }, event) {
-      return EventService.postEvent(event).then(() => {
-        commit('ADD_EVENT', event)
-      })
-    }
-  },
-  getters: {
-    getEventById: state => id => {
-      return state.events.find(event => event.id === id)
-    }
+    ]
   }
 })
